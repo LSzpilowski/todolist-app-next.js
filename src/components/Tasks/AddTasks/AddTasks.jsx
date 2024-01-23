6154adcdfeac87baa4d1f97824700f3cfe607349
LSzpilowski <lszpilowski@gmail.com>
2024-01-23 14:46:12 +0000

    Add AddTasks component

--- src/components/Tasks/AddTasks/AddTasks.jsx
+++ src/components/Tasks/AddTasks/AddTasks.jsx
@@ -4,9 +4,9 @@ import * as S from './AddTasks.styles'
 
 export const AddTasks = () => {
   return (
-    <S.AddTasks>
-      <input type='text' name='inputbox' value="" placeholder='What to do today?' />
-      <input type='submit'  name='submitButton' value='Submit'/>
-    </S.AddTasks>
+    <S.Form>
+      <S.InputText />
+      <S.InputSubmit />
+    </S.Form>
   )
 }
--- src/components/Tasks/AddTasks/AddTasks.styles.js
+++ src/components/Tasks/AddTasks/AddTasks.styles.js
@@ -1,6 +1,36 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const AddTasks = styled.form`
-height: 50px;
-border: 1px solid black;
+export const Form = styled.form.attrs({
+  autoComplete: 'off',
+})`
+display: flex;
+flex-direction: row;
+justify-content: center;
+padding: 15px;
+margin-top: 50px;
+`;
+
+export const InputText = styled.input.attrs({
+  type: 'text',
+  id:'inputText',
+  placeholder:'What to do today?',
+  minLength: '3',
+  required: true,
+})`
+font-size: 2em;
+border-radius: 7px;
+border: 1px solid #7D7D7D;
+color: red;
+padding: 0px 20px;
+`;
+
+export const InputSubmit = styled.input.attrs({
+  type: 'submit',
+  value: 'Add Todo',
+  id:'inputSubmit',
+})`
+font-size: 2em;
+border-radius: 7px;
+border: 1px solid #7D7D7D;
+padding: 0px 20px;
 `;
