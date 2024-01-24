23abd13688e54bc4e981060a9568d36931410f80
LSzpilowski <lszpilowski@gmail.com>
2024-01-24 15:48:15 +0000

    Add main functionality

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -5,6 +5,7 @@ import * as S from './Tasks.styles';
 
 export const Tasks = () => {
 
+const [dones, setDones] = useState([]);
 const [todos, setTodos] = useState([]);
 const [inputValue, setInputValue] = useState('');
 
@@ -13,9 +14,10 @@ function handleChange(e){
 }
 
 function handleSubmit(e){
-  if (inputValue.length >= '3') {
-    e.preventDefault()
-    setTodos([...todos, inputValue])
+    e.preventDefault();
+    const trimmedInput = inputValue.trim();
+  if (trimmedInput.length >= 3) {
+    setTodos([...todos, trimmedInput])
     setInputValue('')
   } else {
      return null;
@@ -28,19 +30,48 @@ function handleDelete(index){
   setTodos(newTodos)
 }
 
+function handleDone(index) {
+  setDones([...dones, todos[index]])
+  const newTodos = [...todos]
+  newTodos.splice(index, 1)
+  setTodos(newTodos)
+}
+
   return(
-    <>
-    <S.Form>
+    <S.Wrapper>
+    <S.Form onSubmit={handleSubmit}>
       <S.InputText value={inputValue} onChange={handleChange}/>
-      <S.InputSubmit onClick={handleSubmit}/>
+      <S.InputSubmit type='submit'/>
     </S.Form>
-    <S.UnorderdList>
-      {todos.map((todo, index) => (
-          <S.LiTask key={index}>{todo}
-          <S.DelButton onClick={() =>handleDelete(index)}>Delete</S.DelButton>
-          </S.LiTask>
-        ))}
-    </S.UnorderdList>
-    </>
+    <S.TaskTable>
+      <S.Fieldset>
+        <S.Legend>
+        Todo's List ({todos.length})
+        </S.Legend>
+        <S.UnorderdList>
+          {todos.map((todo, index) => (
+            <S.LiToDoTasks key={index}>
+            <S.ToDoTask>{index+1+'. '}{todo}</S.ToDoTask>
+            <S.Buttons>
+            <S.DoneButton onClick={() => handleDone(index)}>Done</S.DoneButton>
+            <S.DelButton onClick={() =>handleDelete(index)}>Delete</S.DelButton>
+            </S.Buttons>
+            </S.LiToDoTasks>
+          ))}
+        </S.UnorderdList>
+      </S.Fieldset>
+
+      <S.Fieldset>
+        <S.Legend>What's Done ({dones.length})</S.Legend>
+        <S.UnorderdList>
+          {dones.map((done, index) => (
+            <S.LiDoneTasks key={index}>
+            <S.ToDoTask>{done}</S.ToDoTask>
+            </S.LiDoneTasks>
+          ))}
+        </S.UnorderdList>
+      </S.Fieldset>
+    </S.TaskTable>
+    </S.Wrapper>
   )
 }
--- src/components/Tasks/Tasks.styles.js
+++ src/components/Tasks/Tasks.styles.js
@@ -1,5 +1,22 @@
 import styled from 'styled-components';
 
+// General
+export const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-evenly;
+
+min-height: 80vh;
+margin: 0;
+padding: 0;
+`;
+
+
+
+
+
+
 // AddTasks
 export const Form = styled.form.attrs({
   autoComplete: 'off',
@@ -7,8 +24,6 @@ export const Form = styled.form.attrs({
 display: flex;
 flex-direction: row;
 justify-content: center;
-padding: 15px;
-margin-top: 50px;
 `;
 
 export const InputText = styled.input.attrs({
@@ -16,13 +31,18 @@ export const InputText = styled.input.attrs({
   id:'inputText',
   placeholder:'What to do today?',
   minLength: '3',
-  maxLength: '50',
+  maxLength: '100',
   required: true,
 })`
 font-size: 2em;
 border-radius: 7px;
-border: 1px solid #7D7D7D;
+border: 1px solid #005BA4;
 padding: 0px 20px;
+
+&:hover {
+  background-color: lightblue;
+  transition: 200ms ease-in-out;
+}
 `;
 
 export const InputSubmit = styled.input.attrs({
@@ -32,33 +52,143 @@ export const InputSubmit = styled.input.attrs({
 })`
 font-size: 2em;
 border-radius: 7px;
-border: 1px solid #7D7D7D;
+border: 1px solid #005BA4;
 padding: 0px 20px;
+
+&:hover {
+  background-color: lightblue;
+  transition: 200ms ease-in-out;
+}
 `;
+
+
+
+
+
 
 // DisplayTasks
-export const UnorderdList = styled.ul`
-max-width: 800px;
-min-height: 33px;
-max-height: 400px;
-overflow: auto;
 
-padding: 10px;
-margin: 25px auto;
-list-style-type: none;
-max-items: 10;
-border: 1px solid black;
+export const TaskTable = styled.div`
+width: 90%;
+height: 60vh;
+padding: 15px;
+
+display: flex;
+flex-orientation: row;
+justify-content: space-around;
 `;
 
-export const LiTask = styled.li`
+export const Fieldset = styled.fieldset`
+width: 50%;
+max-width: 600px;
+min-height: 300px;
+max-height: 450px;
+overflow: auto;
+border: 2px solid #005BA4;
+background-color: lightblue;
+`;
+
+export const Legend = styled.legend`
+font-size: 25px;
+font-weight: bold;
+`;
+
+
+
+export const UnorderdList = styled.ul`
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+
 font-size: 1.5em;
+padding: 0;
 margin: 0;
 `;
 
-export const DelButton = styled.button`
-margin: 0 0 0 10px;
-font-size: 0.8em;
-padding: 2px 10px;
+export const LiToDoTasks = styled.li`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+
+list-style-type: bullet;
+margin: 5px;
+padding: 5px 10px;
+background-color: #005BA4;
+color: lightgrey;
+border-radius: 10px;
+
+&:hover {
+    color: white;
+    background: #115CA9;
+    font-weight: bold;
+`;
+
+export const ToDoTask = styled.p`
+list-style-type: bullet;
+margin: 0;
+width: 65%;
+overflow: hidden;
+`;
+
+export const Buttons = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-evenly;
+align-items: center;
+width: 35%;
+
+
+}
+`;
+
+export const DoneButton = styled.button`
+font-size: 0.75em;
+border: 1px solid black;
 border-radius: 5px;
-border: 1px solid grey;
+padding: 5px 15px;
+margin: 0;
+
+&:hover {
+border-radius: 50px;
+background-color: darkorange;
+transition: 300ms ease-in-out;
+`;
+
+export const DelButton = styled.button`
+font-size: 0.75em;
+border: 1px solid black;
+border-radius: 5px;
+padding: 5px 15px;
+margin: 0;
+
+&:hover {
+border-radius: 50px;
+background-color: firebrick;
+transition: 300ms ease-in-out;
+`;
+
+
+export const LiDoneTasks = styled.li`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+
+list-style-type: bullet;
+margin: 5px;
+padding: 5px 10px;
+background-color: darkorange;
+color: black;
+border-radius: 10px;
+
+&:hover {
+    background: orange;
+    font-weight: bold;
+`;
+export const DoneTask = styled.p`
+list-style-type: bullet;
+margin: 0;
+width: 65%;
+overflow: hidden;
 `;
