309dc6af3f1b382702817a143f5eb045118f7836
LSzpilowski <lszpilowski@gmail.com>
2024-01-29 11:06:17 +0000

    Install font-awesome for icons

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import { Footer } from './components/Footer'
 import * as S from './App.styles'
 
 
-
 export const App = () => {
   return (
   <S.Wrapper>
     <Header />
     <Tasks />
     <Footer />
+  
   </S.Wrapper>
   );
 }
