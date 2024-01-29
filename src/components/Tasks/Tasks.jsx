bffa152e58ab9e2f116f4890b3b3577d26517b1d
LSzpilowski <lszpilowski@gmail.com>
2024-01-29 11:05:43 +0000

    Add TaskHistory div and functionality

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -2,76 +2,158 @@ import React from 'react'
 import { useState } from 'react';
 import * as S from './Tasks.styles';
 
-
 export const Tasks = () => {
 
 const [dones, setDones] = useState([]);
 const [todos, setTodos] = useState([]);
+const [historyTasks, setHistoryTasks] = useState([]);
 const [inputValue, setInputValue] = useState('');
+const [isToggled, setIsToggled] = useState(true);
 
 function handleChange(e){
   setInputValue(e.target.value)
 }
 
-function handleSubmit(e){
+function handleSubmit(e) {
     e.preventDefault();
     const trimmedInput = inputValue.trim();
-  if (trimmedInput.length >= 3) {
-    setTodos([...todos, trimmedInput])
-    setInputValue('')
-  } else {
-     return null;
+    if (trimmedInput.length >= 3) {
+      setTodos([...todos, { text: trimmedInput, isChecked: false }]);
+      setInputValue('');
+    } else {
+      return null;
+    }
   }
-}
 
 function handleDelete(index){
+  setHistoryTasks([...historyTasks, todos[index]]);
+
   const newTodos = [...todos]
   newTodos.splice(index, 1)
   setTodos(newTodos)
 }
 
 function handleDone(index) {
-  setDones([...dones, todos[index]])
-  const newTodos = [...todos]
-  newTodos.splice(index, 1)
-  setTodos(newTodos)
+    const task = todos[index];
+    setDones([...dones, { ...task, isChecked: true }]);
+
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+function handleCheckbox(index, checked) {
+    if(!checked) {
+      const doneTask = dones[index];
+      setTodos([...todos, { ...doneTask, isChecked: false }]);
+      const newDones = [...dones];
+      newDones.splice(index, 1);
+      setDones(newDones);
+    } else {
+      // Update the isChecked state for the item
+      const newDones = [...dones];
+      newDones[index].isChecked = true;
+      setDones(newDones);
+    }
+  }
+
+function handleReUseButton(displayedIndex) {
+  // Ensure index is within bounds
+  const originalIndex = Math.max(historyTasks.length - 10, 0) + displayedIndex;
+  if (originalIndex >= 0 && originalIndex < historyTasks.length) {
+    setTodos([...todos, historyTasks[originalIndex]]);
+
+    const newHistoryTasks = [...historyTasks];
+    newHistoryTasks.splice(originalIndex, 1);
+    setHistoryTasks(newHistoryTasks);
+  } else {
+    console.error('Invalid index for history tasks');
+  }
 }
+
+function handleClearHistory() {
+  setHistoryTasks([]);
+}
+
+const onToggle = () => setIsToggled(!isToggled);
+
+const taskHistoryHeight = isToggled ? { height: '20px' } : { height: '200px' }; // Adjust the height values as needed
+const latestHistoryTasks = historyTasks.slice(-10);
 
   return(
     <S.Wrapper>
-    <S.Form onSubmit={handleSubmit}>
+    <S.AddTask onSubmit={handleSubmit}>
       <S.InputText value={inputValue} onChange={handleChange}/>
       <S.InputSubmit type='submit'/>
-    </S.Form>
-    <S.TaskTable>
+    </S.AddTask>
+    <S.DisplayTasks>
       <S.Fieldset>
         <S.Legend>
-        Todo's List ({todos.length})
+        Todo's List {todos.length > 0 ? `(${todos.length})` : ''}
         </S.Legend>
         <S.UnorderdList>
           {todos.map((todo, index) => (
             <S.LiToDoTasks key={index}>
-            <S.ToDoTask>{index+1+'. '}{todo}</S.ToDoTask>
+              <S.TodoContainer>
+                <S.ToDoTask>
+              {index+1+'. '}{todo.text}
+            </S.ToDoTask>
             <S.Buttons>
-            <S.DoneButton onClick={() => handleDone(index)}>Done</S.DoneButton>
-            <S.DelButton onClick={() =>handleDelete(index)}>Delete</S.DelButton>
+              <S.DoneButton onClick={() => handleDone(index)}>Done</S.DoneButton>
+              <S.DelButton onClick={() =>handleDelete(index)}>Delete</S.DelButton>
             </S.Buttons>
+              </S.TodoContainer>
+            
             </S.LiToDoTasks>
           ))}
         </S.UnorderdList>
       </S.Fieldset>
 
       <S.Fieldset>
-        <S.Legend>What's Done ({dones.length})</S.Legend>
+        <S.Legend>
+          <S.LegendText>
+            What's Done {dones.length > 0 ? `(${dones.length})` : ''}
+          </S.LegendText>
+          </S.Legend>
         <S.UnorderdList>
           {dones.map((done, index) => (
             <S.LiDoneTasks key={index}>
-            <S.ToDoTask>{done}</S.ToDoTask>
+              <S.DoneTask>{done.text}
+                <S.DoneCheckbox 
+                  checked={done.isChecked} 
+                  onChange={(e) => handleCheckbox(index, e.target.checked)}
+                />
+              </S.DoneTask>
             </S.LiDoneTasks>
           ))}
         </S.UnorderdList>
       </S.Fieldset>
-    </S.TaskTable>
+    </S.DisplayTasks>
+
+    <S.TaskHistory style={taskHistoryHeight}>
+        <S.Legend>
+          <S.LegendText>Your Task History</S.LegendText>
+          <S.HideShowButton 
+            id="toggle"
+            onClick={onToggle}>
+              {!isToggled? 'Hide' : 'Show'}
+          </S.HideShowButton>
+          <S.ClearButton onClick={handleClearHistory}>Clear History</S.ClearButton>
+        </S.Legend>
+        {!isToggled && (
+          <S.UnorderdList>
+            {latestHistoryTasks.map((task, index) => (
+              <S.LiHistory key={index}>
+                <S.HistoryTask>{task.text}
+                <S.ReUseButton 
+                  onClick={(e) => handleReUseButton(index)}>
+                    Re-Use</S.ReUseButton>
+              </S.HistoryTask>
+            </S.LiHistory>
+            ))}
+          </S.UnorderdList>
+        )}
+      </S.TaskHistory>
     </S.Wrapper>
   )
 }
--- src/components/Tasks/Tasks.styles.js
+++ src/components/Tasks/Tasks.styles.js
@@ -18,7 +18,7 @@ padding: 0;
 
 
 // AddTasks
-export const Form = styled.form.attrs({
+export const AddTask = styled.form.attrs({
   autoComplete: 'off',
 })`
 display: flex;
@@ -68,7 +68,7 @@ padding: 0px 20px;
 
 // DisplayTasks
 
-export const TaskTable = styled.div`
+export const DisplayTasks = styled.div`
 width: 90%;
 height: 60vh;
 padding: 15px;
@@ -89,9 +89,13 @@ background-color: lightblue;
 `;
 
 export const Legend = styled.legend`
+background-color: transparent;
 font-size: 25px;
 font-weight: bold;
+
+transition: 300ms ease-in-out;
 `;
+
 
 
 
@@ -124,10 +128,19 @@ border-radius: 10px;
     font-weight: bold;
 `;
 
+export const TodoContainer = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+
+width: 100%;
+`;
+
+
 export const ToDoTask = styled.p`
-list-style-type: bullet;
+width: 100%;
 margin: 0;
-width: 65%;
 overflow: hidden;
 `;
 
@@ -136,8 +149,10 @@ display: flex;
 flex-direction: row;
 justify-content: space-evenly;
 align-items: center;
+width: inherit;
+margin: 0;
+padding: 0;
 width: 35%;
-
 
 }
 `;
@@ -175,7 +190,110 @@ flex-direction: row;
 justify-content: space-between;
 align-items: center;
 
-list-style-type: bullet;
+margin: 5px;
+padding: 5px 10px;
+background-color: darkorange;
+color: black;
+border-radius: 10px;
+
+&:hover {
+background: orange;
+font-weight: bold;
+`;
+export const DoneTask = styled.s`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+
+margin: 0;
+width: 100%;
+overflow: hidden;
+`;
+
+export const DoneCheckbox = styled.input.attrs({
+  type: 'checkbox',
+  id:'doneCheckbox',
+})`
+
+width: 17px;
+height: 17px;
+`;
+
+
+
+// Styling for TaskHistory 
+
+
+
+
+
+export const TaskHistory = styled.fieldset`
+position: relative;
+width: 60%;
+
+margin: 0;
+border: 2px solid #005BA4;
+
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+
+
+max-height: 450px;
+overflow: auto;
+background-color: lightblue;
+`;
+
+
+export const LegendText = styled.p`
+margin: 0;
+`;
+
+export const HideShowButton = styled.button`
+position: absolute;
+top: -2px;
+right: 10px;
+
+font-size: 0.75em;
+border: 1px solid black;
+border-radius: 5px;
+padding: 5px 15px;
+margin: 0;
+
+&:hover {
+border-radius: 50px;
+background-color: darkorange;
+transition: 300ms ease-in-out;
+`;
+
+export const ClearButton = styled.button`
+position: absolute;
+top: -2px;
+right: 100px;
+
+font-size: 0.75em;
+border: 1px solid black;
+border-radius: 5px;
+padding: 5px 15px;
+margin: 0;
+
+&:hover {
+border-radius: 50px;
+background-color: darkorange;
+transition: 300ms ease-in-out;
+
+`;
+
+
+
+
+export const LiHistory = styled.li`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+
 margin: 5px;
 padding: 5px 10px;
 background-color: darkorange;
@@ -185,10 +303,33 @@ border-radius: 10px;
 &:hover {
     background: orange;
     font-weight: bold;
+
 `;
-export const DoneTask = styled.p`
-list-style-type: bullet;
+export const HistoryTask = styled.p`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+
 margin: 0;
-width: 65%;
+padding: 5px 10px;
+background-color: darkorange;
+color: black;
+border-radius: 10px;
+
+width: 100%;
 overflow: hidden;
+`;
+
+export const ReUseButton = styled.button`
+font-size: 0.75em;
+border: 1px solid black;
+border-radius: 5px;
+padding: 5px 15px;
+margin: 0;
+
+&:hover {
+border-radius: 50px;
+background-color: darkorange;
+transition: 300ms ease-in-out;
 `;
