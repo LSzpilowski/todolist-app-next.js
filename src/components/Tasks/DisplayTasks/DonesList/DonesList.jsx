e3350d6ec638e7ade331cc0f2aa16154a4275ff2
LSzpilowski <lszpilowski@gmail.com>
2024-02-02 19:27:00 +0000

    Add DonesList component

---
+++ src/components/Tasks/DisplayTasks/DonesList/DonesList.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import * as S  from './DonesList.styles';
+import * as U from '../utils.styles';
+
+export const DonesList = ({dones, handleCheckbox}) => {
+  return (
+    <U.Fieldset>
+          <U.Legend>
+            <S.LegendText>
+              What's Done {dones.length > 0 ? `(${dones.length})` : ""}
+            </S.LegendText>
+          </U.Legend>
+          <S.DonesList>
+            {dones.map((done, index) => (
+              <S.LiDoneTasks key={index}>
+                <S.DoneTask>
+                  {done.text}
+                  <S.DoneCheckbox
+                    checked={done.isChecked}
+                    onChange={(e) => handleCheckbox(index, e.target.checked)}
+                  />
+                </S.DoneTask>
+              </S.LiDoneTasks>
+            ))}
+          </S.DonesList>
+        </U.Fieldset>
+  )
+}
---
+++ src/components/Tasks/DisplayTasks/DonesList/DonesList.styles.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { LiTasks, UnorderdList } from "../utils.styles";
+
+
+export const DonesList = styled(UnorderdList)`
+margin: 0;
+min-height: 250px;
+max-height: 380px;
+padding: 5px;
+`;
+
+export const LiDoneTasks = styled(LiTasks)`
+background-color: rgb(255, 165, 0);
+opacity: 0.7;
+color: black;
+&:hover {
+opacity: 1;
+`;
+
+export const DoneTask = styled.s`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  margin: 0;
+  width: 100%;
+  overflow: hidden;
+`;
+
+export const LegendText = styled.p`
+  margin: 0;
+`;
+
+export const DoneCheckbox = styled.input.attrs({
+  type: "checkbox",
+  id: "doneCheckbox",
+})`
+  width: 17px;
+  height: 17px;
+`;
