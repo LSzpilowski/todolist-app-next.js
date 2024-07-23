c3e605595e856b5d24a20a36723c676b993dc1d3
LSzpilowski <lszpilowski@gmail.com>
2024-07-23 12:32:35 +0100

    fix packages and install next/themes

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+  },
+};
